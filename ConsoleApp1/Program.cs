a0460430d545c758902bf2c00955812adea8ca79
User <User@USER-4>
2023-01-21 15:02:32 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿//int num = int.Parse(Console.ReadLine());
+//if (num > 0)
+//    Console.WriteLine(++num);
+//else if (num < 0)
+//    Console.WriteLine(--num);
+//else { Console.WriteLine(num); }
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//if (a < b) { Console.WriteLine("Min" + a); }
+//else {Console.WriteLine("Min" + b);}
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//if ((a != b) && (a > b))
+//{ Console.WriteLine("NATIJA" + a); }
+//else if (a == b)
+//{ Console.WriteLine(0); }
+
+
+
+
+
+
