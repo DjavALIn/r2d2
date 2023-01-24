2c3aa1a38b630a8dcf720fab60a908b5f3b226b9
User <User@USER-4>
2023-01-24 16:17:52 +0500

    this is first commint

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,6 +15,93 @@
 //else if (a == b)
 //{ Console.WriteLine(0); }
 
+//case 2:
+//Console.Write("K = ");
+//double K = double.Parse(Console.ReadLine());
+//switch (K)
+//{
+//    case 1:
+//        Console.WriteLine("yomon");
+//        break;
+//    case 2:
+//        Console.WriteLine("qoniqarsiz");
+//        break;
+//    case 3:
+//        Console.WriteLine("qoniqarli");
+//        break;
+//    case 4:
+//        Console.WriteLine("yahshi");
+//        break;
+//    case 5:
+//        Console.WriteLine("a,lo");
+//        break;
+
+//    default: Console.WriteLine("bunaqa amal yuq"); break;
+//}
+//goto case 2;
+Console.Write("Oy raqamini kiriting = ");
+int Oy = int.Parse(Console.ReadLine());
+switch (Oy)
+{
+    case 1:
+        Console.WriteLine("Qish");
+        break;
+    case 2:
+        Console.WriteLine("Qish");
+        break;
+    case 3:
+        Console.WriteLine("Bahor");
+        break;
+    case 4:
+        Console.WriteLine("Bahor");
+        break;
+    case 5:
+        Console.WriteLine("Bahor");
+        break;
+    case 6:
+        Console.WriteLine("Yoz");
+        break;
+    case 7:
+        Console.WriteLine("Yoz");
+        break;
+    case 8:
+        Console.WriteLine("Yoz");
+        break;
+    case 9:
+        Console.WriteLine("Kuz");
+        break;
+    case 10:
+        Console.WriteLine("Kuz");
+        break;
+    case 11:
+        Console.WriteLine("Kuz");
+        break;
+    case 12:
+        Console.WriteLine("Qish");
+        break;
+
+    default: Console.WriteLine("bunaqa amal yuq"); break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
